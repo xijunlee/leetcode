30f3ea9f895063216f8c2fc19c2fb171c82bcd33
Xi-Jun Lee <xijun.lee@hotmail.com>
2017-03-14 13:27:50 +0800

    476

---
+++ 476.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        i = 1
+        while i <= num:
+            i = i << 1
+        return (i - 1) ^ num
+
+

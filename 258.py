9a26be18c007599db3087547059451d01ced1307
Xi-Jun Lee <xijun.lee@hotmail.com>
2017-03-16 11:25:28 +0800

    258

---
+++ 258.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num % 9 == 0:
+            if num > 0:
+                return 9
+            else:
+                return 0
+        else:
+            return num % 9

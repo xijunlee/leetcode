0ffaaec58f5f578a5de4715657c4f4631daf31b1
XIJUN@MARK-II <xijun.lee@hotmail.com>
2017-03-23 15:38:00 +0800

    169

---
+++ 169.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for item in set(nums):
+            if nums.count(item) > len(nums)/2:
+                return item
+        

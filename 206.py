961af9d14f818532f162dcb6e82645bca0e93d0c
Xi-Jun Lee <xijun.lee@hotmail.com>
2017-03-24 14:16:51 +0800

    206

---
+++ 206.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(set(nums)) != len(nums):
+            return True
+        else:
+            return False
+        

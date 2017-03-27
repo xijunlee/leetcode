43fad5da8b59684a1dc31574f674de1142ac5d18
XIJUN@MARK-II <xijun.lee@hotmail.com>
2017-03-27 13:14:43 +0800

    change 53

--- 53.py
+++ 53.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding=utf-8
-
 class Solution(object):
     def maxSubArray(self, nums):
         """
@@ -9,15 +6,12 @@ class Solution(object):
         """
         f = []
         for i in range(len(nums)):
-            f.append([0 for j in range(len(nums))])
+            f.append(0)
+        f[0] = nums[0]
         ret = -0x7fffffff
         for i in range(0,len(nums)):
-            for j in range(0,i+1):
-                if i != j:
-                    f[i][j] = f[i-1][j] + nums[i]
-                else:
-                    f[i][j] = nums[i]
-                ret = max(ret, f[i][j])
-
+            if i >= 1:
+                f[i] = max(nums[i],f[i-1]+nums[i])
+            ret = max(ret,f[i])
         return ret
                 

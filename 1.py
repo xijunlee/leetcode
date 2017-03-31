696369e9591d4329466462e1f9b2bc99c3d59de4
Xi-Jun Lee <xijun.lee@hotmail.com>
2017-03-31 18:44:06 +0800

    1

--- 1.py
+++ 1.py
@@ -1,24 +1,15 @@
-#coding = utf-8
-import sys
-import pdb
+#!/usr/bin/env python
+# coding=utf-8
 
-if __name__ == "__main__":
-    
-    line = sys.stdin.readline()
-    nk = map(int, line.strip().split())
-    n,k = nk[0],nk[1]
-    line = sys.stdin.readline()
-    values = map(int, line.strip().split())
-    #pdb.set_trace()
-    for s in range(k):
-        ori = values[:]
-        for i in range(n):
-            if i < n-1:
-                values[i]=(ori[i]+ori[i+1])%100
-            else:
-                values[i]=(ori[i]+ori[0])%100
-    ans = ""
-    for i in range(n-1):
-        ans += str(values[i])+ " "
-    ans += str(values[n-1])
-    print ans
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j and nums[i]+nums[j] == target:
+                    return [i,j]
+        

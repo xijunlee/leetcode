bd203708975bb37e30017adc4399245c4e2c9815
XIJUN@MARK-II <xijun.lee@hotmail.com>
2017-04-06 21:19:14 +0800

    401 finally accepted!

--- 401.py
+++ 401.py
@@ -1,35 +1,22 @@
-#!/usr/bin/env python
-# coding=utf-8
-
-import pdb
-
 class Solution(object):
     def readBinaryWatch(self, num):
         ret = []
         if num == 0:
             ret.append('0:00')
+            return ret
 
         for i in range(1024):
-            l = []
-            k = i
-            while k > 0:
-                l.append(k%2)
-                k /= 2
-            if l.count == num:
-                pdb.set_trace()
-                high = i & (15 << 8)
-                low = i & 255
-                if low < 10:
+            count = 0
+            n = i
+            while n:
+                n = n&(n-1)
+                count += 1
+            if count == num:
+                high = (i & (15 << 6)) >> 6
+                low = i & 63
+                if high < 12 and low < 10:
                     ret.append(str(high)+':0'+str(low))
-                else:
+                elif high < 12 and low < 60:
                     ret.append(str(high)+':'+str(low))
         return ret
-
-
-if __name__ == '__main__':
-
-	s = Solution()
-	print type(s)
-	pdb.set_trace()
-	s.readBinaryWatch(1)
-
+        

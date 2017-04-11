e0fa889ba4a6cad9e55697a90682785567cf46bf
XIJUN@MARK-II <xijun.lee@hotmail.com>
2017-04-11 14:02:24 +0800

    39 another solution

--- 39.py
+++ 39.py
@@ -36,6 +36,22 @@ class Solution(object):
         dfs(0,0,[])
         return ret
 
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        res = []
+        candidates.sort()
+        self.dfs(candidates, target, 0, [], res)
+        return res
+    
+    def dfs(self, nums, target, index, path, res):
+        if target < 0:
+            return  # backtracking
+        if target == 0:
+            res.append(path)
+            return 
+        for i in xrange(index, len(nums)):
+            self.dfs(nums, target-nums[i], i, path+[nums[i]], res)
+
 if __name__ == '__main__':
     s = Solution()
     print s.combinationSum([2, 3, 6, 7], 7)
